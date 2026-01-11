8479efa7b4597b1eb9c210ab2f982f07a04debaa
biplabhosen <biplobhosen214@gmail.com>
2026-01-11 12:27:47 +0600

    api

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\CustomerController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::get("customer", CustomerController::class, "index");
+Route::apiResource("customer", CustomerController::class);
